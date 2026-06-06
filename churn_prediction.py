3eb66737c7e6121b2ada01b60b6fd452658bebe9
agnijagansingh07-byte <agnijagansingh07@gmail.com>
2026-06-07 01:49:51 +0530

    Upload Customer Churn Prediction Project

---
+++ churn_prediction.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+print("Customer Churn Prediction Project")
+# Add dataset and train model here
